8a0fab33e903fa6a2af318ad7a2968ff5c095412
Golden Chimusinde <goldenchimsy@gmail.com>
2024-11-13 22:41:09 +0200

    Implement a Global Function for Job Execution
    - testing the function in index route

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Console\BackgroundJobRunner;
+use App\Helpers\BackgroundJobHelper;
+
 
 class HomeController
 {
     public function index()
     {
 
-        $jobRunner = new BackgroundJobRunner();
-        $jobRunner->run('SomeClass', 'someMethod', ['param1', 'param2']);
+//        $jobRunner = new BackgroundJobRunner();
+//        $jobRunner->run('App\Services\SomeClass', 'someMethod', ['param1', 'param2']);
 
+        // Run the background job
+//        BackgroundJobHelper::runBackgroundJob('App\Services\SomeClass', 'someMethod', ['param1', 'param2']);
 
         return view('home');
     }
