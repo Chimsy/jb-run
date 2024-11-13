5bee90256fead6b84d70241eac06a056b95f66bc
Golden Chimusinde <goldenchimsy@gmail.com>
2024-11-13 21:30:03 +0200

    Design a Background Job Runner System
    - added namespaces to web.php to silence the IDE
    - tested if the Background Script is running as expected

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Console\BackgroundJobRunner;
+
 class HomeController
 {
     public function index()
     {
+
+        $jobRunner = new BackgroundJobRunner();
+        $jobRunner->run('SomeClass', 'someMethod', ['param1', 'param2']);
+
+
         return view('home');
     }
 }
